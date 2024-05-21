3e7b86846a9545138af64cc92657fcc5253ab641
roh <rohilkie94@gmail.com>
2024-05-21 17:12:52 +0900

    수정

--- src/pages/FileDrop.tsx
+++ src/pages/FileDrop.tsx
@@ -12,14 +12,13 @@ export default function FileDrop() {
         console.log(`file[${i}]`, file)
       }
     }
-
-    return (
-      <div>
-        <p>FileDrop</p>
-        <div onDrop={onDrop} onDragOver={onDragOver}>
-          <h1>Drop image files over Me</h1>
-        </div>
-      </div>
-    )
   }
+  return (
+    <div>
+      <p>FileDrop</p>
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drop image files over Me</h1>
+      </div>
+    </div>
+  )
 }
